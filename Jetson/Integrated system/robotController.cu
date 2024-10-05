28c4a601ce9cc6a91a521decd23078eb00c55cab
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-05 12:22:44 +0200

    compile fix

--- Jetson/Integrated system/robotController.cu
+++ Jetson/Integrated system/robotController.cu
@@ -341,11 +341,16 @@ std::vector<float> robotController::getDistanceMeasurements()
 {
     // Measure distance from each sensor with a 5ms delay between each measurement
     std::vector<float> distances;
-    for (int i = 0; i < 5; i++)
-    {
-        distances.push_back(serial.receiveDistanceSensorMeasurement(i));
-        delay(5);
-    }
+    distances.push_back(serial.getDistance(DistSense::SENSE_1));
+    delay(5);
+    distances.push_back(serial.getDistance(DistSense::SENSE_2));
+    delay(5);
+    distances.push_back(serial.getDistance(DistSense::SENSE_3));
+    delay(5);
+    distances.push_back(serial.getDistance(DistSense::SENSE_4));
+    delay(5);
+    distances.push_back(serial.getDistance(DistSense::SENSE_5));
+    delay(5);
 
     return distances;
 }
