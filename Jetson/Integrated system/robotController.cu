40b25224aea7e0c8aa4a705c46d7abe9fe9530c3
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-10 15:12:11 +0200

    update

--- Jetson/Integrated system/robotController.cu
+++ Jetson/Integrated system/robotController.cu
@@ -240,6 +240,7 @@ void robotController::moveAndDetect()
         // Check if an obstacle has been detected, if so we need to call the explore function to get a new setpoint
         if (!canMoveForwards())
         {
+            std::cout << "!" << std::endl;
             std::vector<float> goalPosition = this->navigation.explore(this->robotPosition, this->distanceMeasurements);
             this->positionController.setGoal(goalPosition[0], goalPosition[1], goalPosition[2]);
         }
