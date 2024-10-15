1fa9bafeae3d04c6a095d3dbb6a0ffcfd67294b0
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-15 12:00:33 +0200

    update

--- Jetson/Integrated system/marker/contourDetector.cuh
+++ Jetson/Integrated system/marker/contourDetector.cuh
@@ -8,18 +8,17 @@
 
 #define DISTANCE_THRESHOLD 30
 
-class contourDetector {
+class contourDetector
+{
 public:
     contourDetector();
     ~contourDetector();
 
-    static std::vector<std::vector<int>> contourDetector::detect(const float *image, int width, int height);
+    static std::vector<std::vector<int>> detect(const float *image, int width, int height);
 
 private:
     static bool isValid(int x, int y, int width, int height);
     static float calculateDistance(float x1, float y1, float x2, float y2);
 };
 
-
-
-#endif //CONTOURDETECTOR_CUH
+#endif // CONTOURDETECTOR_CUH
