71fdbfedb6ac10fce75e3de6694715586074ca4e
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-15 11:36:23 +0200

    update

--- Jetson/Integrated system/marker/RDP.cu
+++ Jetson/Integrated system/marker/RDP.cu
@@ -6,27 +6,32 @@
 
 #include <algorithm>
 
-RDP::RDP() {
+RDP::RDP()
+{
 }
 
-RDP::~RDP() {
+RDP::~RDP()
+{
 }
 
-std::vector<std::tuple<int, int> > RDP::ramerDouglasPeucker(const std::vector<std::tuple<int, int> > &contour,
-                                                            const float epsilon) {
+std::vector<std::tuple<int, int>> RDP::ramerDouglasPeucker(const std::vector<std::tuple<int, int>> &contour,
+                                                           const float epsilon)
+{
     // Find the point with the maximum distance
-    auto startPoint = contour[0];
-    auto endPoint = contour[contour.size() - 1];
+    std::tuple<int, int> startPoint = contour[0];
+    std::tuple<int, int> endPoint = contour[contour.size() - 1];
 
     float maxDistance = 0;
     int index = -1;
 
-    for (int i = 1; i < contour.size() - 1; i++) {
+    for (int i = 1; i < contour.size() - 1; i++)
+    {
         auto point = contour[i];
 
         auto distance = perpendicularDistance(point, startPoint, endPoint);
 
-        if (distance > maxDistance) {
+        if (distance > maxDistance)
+        {
             index = i;
             maxDistance = distance;
         }
@@ -34,7 +39,8 @@ std::vector<std::tuple<int, int> > RDP::ramerDouglasPeucker(const std::vector<st
 
     // If the maximum distance is greater than epsilon, recursively simplify
     std::vector<std::tuple<int, int>> result;
-    if (maxDistance > epsilon) {
+    if (maxDistance > epsilon)
+    {
         // Recursive call for the two segments
         const std::vector firstHalf(contour.begin(), contour.begin() + index + 1);
         const std::vector secondHalf(contour.begin() + index, contour.end());
@@ -48,7 +54,9 @@ std::vector<std::tuple<int, int> > RDP::ramerDouglasPeucker(const std::vector<st
 
         // Append result2 to result
         result.insert(result.end(), result2.begin(), result2.end());
-    } else {
+    }
+    else
+    {
         // If the distance is below the threshold, return the endpoints only
         result.push_back(startPoint);
         result.push_back(endPoint);
@@ -57,15 +65,17 @@ std::vector<std::tuple<int, int> > RDP::ramerDouglasPeucker(const std::vector<st
     return result;
 }
 
-
-float RDP::arcLength(const std::vector<std::tuple<int, int>> &contour) {
+float RDP::arcLength(const std::vector<std::tuple<int, int>> &contour)
+{
     const auto startPoint = contour[0];
 
     // Calculate the euclideanDistance between each point in the contour and add them together
     std::tuple<int, int> prevPoint = startPoint;
     float distance = 0;
-    for (auto point: contour) {
-        if (point == prevPoint) {
+    for (auto point : contour)
+    {
+        if (point == prevPoint)
+        {
             continue;
         }
 
@@ -81,7 +91,8 @@ float RDP::arcLength(const std::vector<std::tuple<int, int>> &contour) {
 }
 
 float RDP::perpendicularDistance(const std::tuple<int, int> &point, const std::tuple<int, int> &start,
-                                 const std::tuple<int, int> &end) {
+                                 const std::tuple<int, int> &end)
+{
     // Extract point coordinates
     const int x = std::get<0>(point);
     const int y = std::get<1>(point);
@@ -95,7 +106,8 @@ float RDP::perpendicularDistance(const std::tuple<int, int> &point, const std::t
     // Calculate the area of the triangle formed by the line and the point
     // and divide by the length of the base (line segment)
 
-    if (x1 == x2 && y1 == y2) {
+    if (x1 == x2 && y1 == y2)
+    {
         return sqrt(pow((x - x1), 2) + pow((y - y1), 2));
     }
 
@@ -105,13 +117,13 @@ float RDP::perpendicularDistance(const std::tuple<int, int> &point, const std::t
     return denominator == 0 ? 0 : numerator / denominator; // Avoid div by 0
 }
 
-float RDP::euclideanDistance(const std::tuple<int, int> &start, const std::tuple<int, int> &end) {
+float RDP::euclideanDistance(const std::tuple<int, int> &start, const std::tuple<int, int> &end)
+{
     const int x1 = std::get<0>(start);
     const int y1 = std::get<1>(start);
 
     const int x2 = std::get<0>(end);
     const int y2 = std::get<1>(end);
-
 
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
