846393896afc15c483af1d384ab4e1d81e5296cb
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-15 11:42:42 +0200

    update

--- Jetson/Integrated system/marker/RDP.cuh
+++ Jetson/Integrated system/marker/RDP.cuh
@@ -5,18 +5,19 @@
 #ifndef RDP_CUH
 #define RDP_CUH
 #include <vector>
-
-class RDP {
+#include <tuple>
+class RDP
+{
 public:
     RDP();
     ~RDP();
 
     static std::vector<std::tuple<int, int>> ramerDouglasPeucker(const std::vector<std::tuple<int, int>> &contour, float epsilon);
     static float arcLength(const std::vector<std::tuple<int, int>> &contour);
+
 private:
     static float perpendicularDistance(const std::tuple<int, int> &point, const std::tuple<int, int> &start, const std::tuple<int, int> &end);
     static float euclideanDistance(const std::tuple<int, int> &start, const std::tuple<int, int> &end);
 };
 
-
-#endif //RDP_CUH
+#endif // RDP_CUH
