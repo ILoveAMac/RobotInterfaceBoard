9a047357adb827a7201732a00f2a7c90a5d441a9
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-17 16:06:31 +0200

    update

--- Jetson/Integrated system/robotController.cu
+++ Jetson/Integrated system/robotController.cu
@@ -833,7 +833,7 @@ void robotController::rotateForTranslation()
     }
     else
     {
-        float newAngle = this->robotPosition[2] + (M_PI / 2) + this->calculatedYaw;
+        float newAngle = this->robotPosition[2] - (M_PI / 2) + fabs(this->calculatedYaw);
         // Normalize the angle
         while (newAngle > M_PI)
         {
