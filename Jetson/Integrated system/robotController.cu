0afbc066c5a77e8bff1b181c86e6a8e97b01874f
ILoveAMac <jlp.roets@tuks.co.za>
2024-10-18 13:12:27 +0200

    update

--- Jetson/Integrated system/robotController.cu
+++ Jetson/Integrated system/robotController.cu
@@ -704,7 +704,7 @@ void robotController::allignToMarker()
 
     this->calculatedYaw = yaw;
     this->distanceFromMarker = std::get<0>(markerVectors)[2];
-    this->distanceToTranslate = fabs(this->distanceFromMarker * tan((M_PI / 2) + yaw));
+    this->distanceToTranslate = fabs(this->distanceFromMarker * tan(yaw));
 
     // 3. If we are not in allignment, we have to translate the robot horozontally
     //    -- Translate by distanceFromMarker * tan(yaw)
